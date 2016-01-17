8466c90272d70a910289488481b63fd2599e2291
gidmon <per.gidlund@gmail.com>
2016-01-17 20:40:49 +0100

    Quick and dirty first version

---
+++ test/app.ts
@@ -0,0 +1,6 @@
+///<reference path="../typings/mocha/mocha.d.ts" />
+///<reference path="../typings/chai/chai.d.ts" />
+import chai = require('chai')
+var expect = chai.expect;
+
+import app = require('../app')
