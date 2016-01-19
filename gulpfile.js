9df4a4c66bdfcede861c05dda2b3a8464cd257de
gidmon <per.gidlund@gmail.com>
2016-01-19 10:08:25 +0100

    Changes in monitor view

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 gulp.task('build', function () {
   return gulp
-    .src(['views/*.html','scripts/*.js', 'node_modules/bootstrap/dist/*/*.*', 'package.json'], { base: './' })
+    .src(['views/*.html','scripts/*.js', 'node_modules/bootstrap/dist/*/*.*','node_modules/angular//*.*', 'package.json'], { base: './' })
     .pipe(gulp.dest('build'))
 });
 
