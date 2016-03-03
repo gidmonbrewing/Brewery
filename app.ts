f282452339e6f89ba4e004ed0dabcbd52d0e9eba
Daniel <daniel.hermansson@gmail.com>
2016-03-03 19:22:20 +0100

    la till kod för relät

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express = require('express');
 var sensor = require('ds18x20');
+var gpio = require('pi-gpio');
 var app = express();
 var server = app.listen(8334, function(){
     console.log("Lyssnar");
@@ -13,6 +14,33 @@ app.use(express.static('node_modules'))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+gpio.close(22, function(er){
+console.log("close");
+console.log(er);
+
+op();
+
+});
+
+function op(){
+	gpio.open(22, "output", function(err){
+	console.log("open");
+	console.log(err);
+	gpio.write(22, 1, function(err){
+	console.log("write" + err);	
+	});
+	});
+}
+
+function write(){
+	gpio.write(22, false, function(err){
+	if(err)
+		console.log(err);
+	});
+	console.log('gpio write')
+};
+
+
 var isLoaded = sensor.isDriverLoaded();
 console.log(isLoaded);
 if(!isLoaded) {
@@ -24,8 +52,11 @@ if(!isLoaded) {
     }
 }
 
- var sensorMapping = [];
+var sensorMapping = [];
 sensorMapping["28-0000072e90ed"] = "Demosensor(baren)";
+sensorMapping["28-000007308286"] = "HLT (Ut)";
+sensorMapping["28-0000072fbb77"] = "MSK (In)";
+sensorMapping["28-0000072e1eae"] = "MSK (Ut)";
 
 var devices;
 try {
@@ -34,12 +65,39 @@ try {
     console.log(ex);
 }
 var debug = false;
+var heatOn = false;
+var targetTemp = 30;
 
 // Send current time to all connected clients
 function sendTemp() {
     try {
     for(var i = 0; i<devices.length;i++){
       io.emit('temp', { id: devices[i], name: sensorMapping[devices[i]], temp: sensor.get(devices[i])});  
+    
+        if(sensorMapping[devices[i]] == "MSK (In)")
+	   {
+		console.log(heatOn);
+		var mskintemp = sensor.get(devices[i]);
+		if(!heatOn)
+		{
+			gpio.write(22, 0, function(err){
+				if(err)
+					console.log(err);
+				});
+			console.log("heatOn = true");
+			heatOn = true;
+			console.log(heatOn);
+		}
+		else if(heatOn)
+		{
+			gpio.write(22, 1, function(err){
+			console.log(err);
+			});
+			heatOn = false;
+		}
+	}    
+
+
     }
     } catch(ex) {
         console.log(ex);
